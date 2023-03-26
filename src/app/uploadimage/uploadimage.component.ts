1db32513720442421722940359d59cd79d9025b6
Singh, P.(Pradeep) <pradeeps@nedbank.co.za>
2023-03-26 11:14:59 +0530

    jhjhjh

--- src/app/uploadimage/uploadimage.component.ts
+++ src/app/uploadimage/uploadimage.component.ts
@@ -138,7 +138,8 @@ export class UploadimageComponent implements OnInit, OnDestroy {
     const index0 = formatedResponse.indexOf('JSON Output:```');
     const index0lower = formatedResponse.indexOf('JSON output:```');
     const output = formatedResponse.indexOf('Output:');
-    const bracket = formatedResponse.indexOf('[');
+    const bracketOpen = formatedResponse.indexOf('[');
+    const bracketClose = formatedResponse.indexOf(']');
     try {
       this.gptResponse = JSON.parse(formatedResponse);
       if (this.gptResponse.dishes) {
@@ -161,9 +162,9 @@ export class UploadimageComponent implements OnInit, OnDestroy {
           const totalLength = `${length + output}`
           this.gptResponse = JSON.parse(formatedResponse.slice(+totalLength));
   
-        } else if (bracket > -1) {
-          const totalLength = `${bracket}`
-          this.gptResponse = JSON.parse(formatedResponse.slice(+totalLength));
+        } else if (bracketOpen > -1) {
+          const close = `${bracketOpen + 1}`
+          this.gptResponse = JSON.parse(formatedResponse.slice(bracketOpen, +close));
         } else {
           this.gptResponseStr = message;
         }
